b7c2cb446f36d2fa297364076490a140380c7757
André Ferreira <contato@andreferreira.me>
2021-11-27 17:31:51 -0300

    Lint file and add truffle-contract-size

--- truffle-config.js
+++ truffle-config.js
@@ -1,32 +1,47 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
 
-require('dotenv').config()
+require("dotenv").config();
 const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+    },
+  },
+  optimizer: {
+    enabled: true,
+    runs: 100,
+  },
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*",
     },
     bsc_testnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
     },
     matic: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
     },
   },
+  plugins: ["truffle-contract-size"],
 };
