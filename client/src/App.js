5b0c459118305627f7d60d6805bbca9206f070a7
André Ferreira <contato@andreferreira.me>
2021-11-28 20:41:10 -0300

    Add ERC20Interaction component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Button from "react-bootstrap/Button";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Alert from "react-bootstrap/Alert";
+import InputGroup from "react-bootstrap/InputGroup";
 
 import "./App.css";
 
@@ -90,7 +91,7 @@ const App = () => {
             contractType="ERC20"
           />
           <hr></hr>
-          <ContractInteraction contract={deployedContractAddress} />
+          <ERC20Interaction web3={web3} address={deployedContractAddress} />
         </Tab>
         {/*  ERC721 Tab */}
         <Tab eventKey="erc721" title="ERC721">
@@ -122,6 +123,9 @@ const TokenParamsForm = (props) => {
     const tx = await contract.methods
       .deployContract(contractType, tokenName, tokenSymbol)
       .send({ from: accounts[0] });
+
+    // TODO: Add loading while waiting for transaction to be mined
+    // TODO: Show notification when transaction is mined
 
     // Get the value from the contract to prove it worked.
     // const response = await contract.methods.get().call();
@@ -162,18 +166,141 @@ const TokenParamsForm = (props) => {
   );
 };
 
-const ContractInteraction = (props) => {
-  const { contract } = props;
+const ERC20Interaction = (props) => {
+  const { web3, address } = props;
+  const [contractAddress, setContractAddress] = useState("");
+  const [name, setName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [totalSupply, setTotalSupply] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [transferAddressTo, setTransferAddressTo] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
+
+  useEffect(() => {
+    setContractAddress(address);
+  }, [address]);
+
+  useEffect(() => {
+    loadContractData();
+  });
+
+  const onChangeContractAddress = (e) => {
+    setContractAddress(e.target.value);
+  };
+
+  const onChangeTransferAddressTo = (e) => {
+    setTransferAddressTo(e.target.value);
+  };
+
+  const onChangeTransferAmount = (e) => {
+    setTransferAmount(e.target.value);
+  };
+
+  const loadContractData = () => {
+    const instance = new web3.eth.Contract(ERC20Contract.abi, contractAddress);
+    if (instance._address) {
+      instance.methods
+        .name()
+        .call()
+        .then((name) => {
+          setName(name);
+        })
+        .catch((error) => {
+          console.log("error :>> ", error);
+        });
+
+      instance.methods
+        .owner()
+        .call()
+        .then((owner) => {
+          setOwner(owner);
+        })
+        .catch((error) => {
+          console.log("error :>> ", error);
+        });
+
+      instance.methods
+        .totalSupply()
+        .call()
+        .then((totalSupply) => {
+          setTotalSupply(totalSupply);
+        })
+        .catch((error) => {
+          console.log("error :>> ", error);
+        });
+
+      instance.methods
+        .symbol()
+        .call()
+        .then((symbol) => {
+          setSymbol(symbol);
+        })
+        .catch((error) => {
+          console.log("error :>> ", error);
+        });
+    }
+  };
+
   return (
     <>
       <h2>Contract Interaction</h2>
-      <Alert variant="primary">
-        {contract ? (
-          <b>Deployed Contract: {contract}</b>
-        ) : (
-          <b>No contract deployed yet</b>
-        )}
-      </Alert>
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridContractAddress">
+            <Form.Label>Contract Address</Form.Label>
+
+            <Form.Control
+              value={contractAddress}
+              onChange={onChangeContractAddress}
+              placeholder="0x00000000000000000000"
+            />
+            <Button variant="primary" onClick={loadContractData}>
+              Load
+            </Button>
+          </Form.Group>
+        </Row>
+      </Form>
+      <p>Token Name: {name}</p>
+      <p>Contract Owner: {owner}</p>
+      <p>
+        Total Supply: {totalSupply} {symbol}
+      </p>
+
+      <hr />
+
+      <h3>Transfer</h3>
+
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridAddressTo">
+            <Form.Label>Address To:</Form.Label>
+
+            <Form.Control
+              value={transferAddressTo}
+              onChange={onChangeTransferAddressTo}
+              placeholder="0x00000000000000000000"
+            />
+            <InputGroup className="mb-3">
+              <Form.Control
+                value={transferAmount}
+                onChange={onChangeTransferAmount}
+                placeholder={"0.01 " + symbol}
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Text id="basic-addon2">{symbol}</InputGroup.Text>
+            </InputGroup>
+            <Button variant="primary" onClick={loadContractData}>
+              Transfer {transferAmount} {symbol}
+            </Button>
+          </Form.Group>
+        </Row>
+      </Form>
+
+      {/* 'balanceOf(address)': [Function: bound _createTxObject],
+    
+    'transfer(address,uint256)': [Function: bound _createTxObject], */}
+      {/* Event Transfer */}
     </>
   );
 };
